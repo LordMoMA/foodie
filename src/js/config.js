4570154fa47577b25dc11b26a8fb00fdb7bf88f6
David Lee <lihongyi0512@gmail.com>
2022-09-25 23:54:31 +0800

    api

--- src/js/config.js
+++ src/js/config.js
@@ -1,5 +1,5 @@
-export const API_URL = 'https://forkify-api.herokuapp.com/api/v2/recipes/';
+export const API_URL = "https://forkify-api.herokuapp.com/api/v2/recipes/";
 export const TIMEOUT_SEC = 10;
 export const RES_PER_PAGE = 10;
-export const KEY = '<YOUR_KEY>';
+export const KEY = "3009a020-88f5-496f-a9c3-a378431416d5";
 export const MODAL_CLOSE_SEC = 2.5;
