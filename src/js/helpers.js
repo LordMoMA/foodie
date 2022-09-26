2013d7f0fd5105a6a4ef4dcf99fb4d788746b282
David Lee <lihongyi0512@gmail.com>
2022-09-26 15:57:44 +0800

    sass

--- src/js/helpers.js
+++ src/js/helpers.js
@@ -1,5 +1,5 @@
-import { async } from 'regenerator-runtime';
-import { TIMEOUT_SEC } from './config.js';
+import { async } from "regenerator-runtime";
+import { TIMEOUT_SEC } from "./config.js";
 
 const timeout = function (s) {
   return new Promise(function (_, reject) {
@@ -13,9 +13,9 @@ export const AJAX = async function (url, uploadData = undefined) {
   try {
     const fetchPro = uploadData
       ? fetch(url, {
-          method: 'POST',
+          method: "POST",
           headers: {
-            'Content-Type': 'application/json',
+            "Content-Type": "application/json",
           },
           body: JSON.stringify(uploadData),
         })
@@ -30,38 +30,3 @@ export const AJAX = async function (url, uploadData = undefined) {
     throw err;
   }
 };
-
-/*
-export const getJSON = async function (url) {
-  try {
-    const fetchPro = fetch(url);
-    const res = await Promise.race([fetchPro, timeout(TIMEOUT_SEC)]);
-    const data = await res.json();
-
-    if (!res.ok) throw new Error(`${data.message} (${res.status})`);
-    return data;
-  } catch (err) {
-    throw err;
-  }
-};
-
-export const sendJSON = async function (url, uploadData) {
-  try {
-    const fetchPro = fetch(url, {
-      method: 'POST',
-      headers: {
-        'Content-Type': 'application/json',
-      },
-      body: JSON.stringify(uploadData),
-    });
-
-    const res = await Promise.race([fetchPro, timeout(TIMEOUT_SEC)]);
-    const data = await res.json();
-
-    if (!res.ok) throw new Error(`${data.message} (${res.status})`);
-    return data;
-  } catch (err) {
-    throw err;
-  }
-};
-*/
